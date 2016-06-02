80a4319739bffef1c523bd74cbf2c1e62eca06f8
alexahn917 <alexahn917@gmail.com>
2016-06-02 15:59:25 -0400

    opti_code
    sample

---
+++ Phase 1/optimization_code.js
@@ -0,0 +1,15 @@
+function optimizeToGrid(num_nodes) {
+	var n = 1;
+	while (num_nodes > Math.pow(n,2)) {
+		n++;
+	}
+
+	var m = n;
+	while (n*m > num_nodes) {
+		m--;
+	}
+
+	var gridArray = [n,m];
+
+	return gridArray;
+}
