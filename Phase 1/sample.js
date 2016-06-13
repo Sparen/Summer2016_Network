000dd8f9b9a40154407899d70dbcaa0bd453ce7d
alexahn917 <alexahn917@gmail.com>
2016-06-13 10:36:28 -0700

    Revert "Separation of codes between drawing and optimizing"
    This reverts commit 93043d6b256500f7c393f4f93ac8d8d5aaf7d5ed.
    
    # Conflicts:
    #	Phase 1/sample.js

--- Phase 1/sample.js
+++ Phase 1/sample.js
@@ -13,23 +13,21 @@ jsPlumb.ready(function() {
             }
         }
     };
+
     client.send();
 });
 
 function initializeNodes() {
     var nodes = database_obj.nodes; 
-    var edges = database_obj.edges;
+    var edges = database_obj.edges; 
 
     jsPlumb.setContainer($('#container'));
 
-    var grid_size = optimizeToGrid(nodes.length);
-    var coord_array = setCoordinates(nodes, edges);
-
+    var coord_array = optimizeToGrid(nodes.length);
 
     for (i = 0; i < nodes.length; i++) {
         var itemheight = 40;
         var newState = $('<div>').addClass('item');
-
         var title = $('<div>').attr('id', nodes[i].id).addClass('title').text(nodes[i].name);
         newState.append(title);
 
@@ -37,11 +35,10 @@ function initializeNodes() {
             var connect = $('<div>').attr('id', nodes[i].columns[j].id).addClass('connect').text(nodes[i].columns[j].name);
             newState.append(connect);
             itemheight += 20;
-       }
+        }
 
-        //Drawing nodes by the coordinates
-        var x_coord = coord_array[i][0];
-        var y_coord = coord_array[i][1];
+        var x_coord = (i % coord_array[0])*150;
+        var y_coord = (Math.floor(i / coord_array[0]))*150;
         placeNodes(newState, itemheight, nodes[i].width, x_coord, y_coord);
         
         $('#container').append(newState); 
@@ -54,7 +51,6 @@ function initializeNodes() {
 
     }
 
-    //Connecting edges
     for (i = 0; i < edges.length; i++) { 
         jsPlumb.connect({
             connector: ["Flowchart", {stub: 20, cornerRadius: 5}],
@@ -62,10 +58,7 @@ function initializeNodes() {
             target: edges[i].target,
             anchor: ["Left", "Right"],
             endpoint: ["Dot", { radius: 5 }],
-<<<<<<< HEAD
-=======
             ConnectionsDetachable: false
->>>>>>> origin/master
         });
     }
 
@@ -84,30 +77,4 @@ function placeNodes(newState, itemheight, width, x, y) {
             'left': x+100, //Temporary static positioning.
             'top' : y+40 //
         });
-}
-
-function setCoordinates(edges, nodes) {
-        var grid_size = optimizeToGrid(nodes.length);
-        //Create 2D array
-        var coord_array = [];
-        for (i = 0; i < nodes.length; i++) {
-            coord_array[i] = [];
-            var x_coord = (i % grid_size[0])*150;
-            var y_coord = (Math.floor(i / grid_size[0]))*150;    
-            coord_array[i][0] = x_coord;
-            coord_array[i][1] = y_coord;              
-        }
-    return coord_array;
-}
-
-
-function hasTarget(id, edges) {
-    numSource = 0;
-    numTarget = 0;
-    for (i = 0; i < edges.length; i++) { 
-        if (edges[i].source == id) {
-            return true;
-        }
-    }
-    return false;
 }
