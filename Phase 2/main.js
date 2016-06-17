f647071a8bd11656c5efa84d9d83d033b721889e
Andrew Fan <andrewsuicune@gmail.com>
2016-06-17 13:39:55 -0400

    Organization improvements and update + draw

--- Phase 2/main.js
+++ Phase 2/main.js
@@ -1,6 +1,8 @@
 var database_obj;
 var allnodes = [];
 
+/* ***** INITIALIZATION AND CANVAS FUNCTIONS ***** */
+
 function start(canvasid) {
     console.log("start(): Running on canvas with id " + canvasid);
     myCanvas.start(canvasid); //set up canvas and interval, etc.
@@ -13,12 +15,33 @@ var myCanvas = {
         this.canvas = document.getElementById(canvasid);
         this.context = this.canvas.getContext("2d");
         this.frameNo = 0;
-        this.interval = setInterval(update_main, 10, canvasid); //in milliseconds. Runs update every 20 millis (25 FPS). canvasid is passed to update_main
+        this.interval = setInterval(update_main, 10, canvasid); //in milliseconds. Runs update every 10 millis (25 FPS). canvasid is passed to update_main
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 };
+
+//Main update loop. Calls the update loops of all objects
+function update_main(canvasid) {
+    myCanvas.clear(); //Begin by clearing everything
+    myCanvas.frameNo += 1; //Increment the master counter
+
+    var i;
+    for (i = 0; i < allnodes.length; i += 1) {
+        allnodes[i].update();
+    }
+
+    draw_main(canvasid); //draw updated things 
+}
+
+//Main draw loop. Handles render order.
+function draw_main(canvasid) {
+    var i;
+    for (i = 0; i < allnodes.length; i += 1) {
+        allnodes[i].draw();
+    }
+}
 
 function onLoad() {
     var filepath = "survey.json";
@@ -42,6 +65,8 @@ function resetCanvas(){
     initializeNodes();
 }
 
+/* ***** NODE HANDLING FUNCTIONS ***** */
+
 function initializeNodes() {
     nodes = database_obj.nodes; 
     edges = database_obj.edges; 
@@ -52,6 +77,7 @@ function initializeNodes() {
     //Scaled coordinates
     var scaled_coord_array = scaleCoordinates(coord_array);
 
+    var i;
     for (i = 0; i < nodes.length; i++) { //for every node in survey.json
         var itemheight = 40;
         //TODO
@@ -68,6 +94,7 @@ function initializeNodes() {
 }
 
 function connectNodes() {
+    var i;
     for (i = 0; i < edges.length; i++) { 
         //TODO
     }    
@@ -75,16 +102,17 @@ function connectNodes() {
 
 //Determines optimized coordinates for each node in a network
 function setCoordinates() {
-        var grid_size = optimizeToGrid(nodes.length);
-        //Create 2D array
-        var coord_array = [];
-        for (i = 0; i < nodes.length; i++) {
-            coord_array[i] = [];
-            var x_coord = (i % grid_size[0]);
-            var y_coord = (Math.floor(i / grid_size[0]));    
-            coord_array[i][0] = x_coord;
-            coord_array[i][1] = y_coord;
-        }
+    var grid_size = optimizeToGrid(nodes.length);
+    //Create 2D array
+    var coord_array = [];
+    var i;
+    for (i = 0; i < nodes.length; i++) {
+        coord_array[i] = [];
+        var x_coord = (i % grid_size[0]);
+        var y_coord = (Math.floor(i / grid_size[0]));    
+        coord_array[i][0] = x_coord;
+        coord_array[i][1] = y_coord;
+    }
     return coord_array;
 }
 
@@ -95,7 +123,8 @@ function scaleCoordinates(coord_array) {
     var y_max = 0;
 
     // return maximum values of x and y
-    for (i=0; i < coord_array.length; i++) {
+    var i;
+    for (i = 0; i < coord_array.length; i++) {
         newCoordinates[i] = [];
         if (coord_array[i][0] >= x_max) {
             x_max = coord_array[i][0];
@@ -107,7 +136,7 @@ function scaleCoordinates(coord_array) {
     }
 
     // scaling by proportion (by maximum valus of x and y)
-    for (i=0; i < coord_array.length; i++) {
+    for (i = 0; i < coord_array.length; i++) {
         newCoordinates[i][0] = coord_array[i][0]/x_max * 350 + 100; // random constant scaling
         newCoordinates[i][1] = coord_array[i][1]/y_max * 150 + 50; // random constant scaling
     }
@@ -118,6 +147,7 @@ function scaleCoordinates(coord_array) {
 function hasTarget(id) {
     var numSource = 0;
     var numTarget = 0;
+    var i;
     for (i = 0; i < edges.length; i++) { 
         if (edges[i].source == id) {
             return true;
