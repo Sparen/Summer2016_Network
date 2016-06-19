509bf11d220a2fcb8a195ab30ba5df8176f52114
alexahn917 <alexahn917@gmail.com>
2016-06-19 14:21:05 -0400

    separeted optimization codes
    might want to separete draw part as well

--- Phase 2/main.js
+++ Phase 2/main.js
@@ -92,6 +92,7 @@ function initializeNodes() {
     //Scaled coordinates
     var scaled_coord_array = scaleCoordinates(coord_array);
 
+    //pushAllNodes
     var i;
     for (i = 0; i < nodes.length; i++) { //for every node in survey.json
         var x_coord = scaled_coord_array[i][0];
@@ -101,10 +102,10 @@ function initializeNodes() {
     }
 
     //Draws edges between nodes
-    connectNodes();
+    pushAllEdges();
 }
 
-function connectNodes() {
+function pushAllEdges() {
     var i;
     for (i = 0; i < edges.length; i++) { 
         var j;
@@ -236,60 +237,4 @@ function setEdgeParameters(edge){
         ctx.lineTo(targetx, targety);
         ctx.stroke();
     }
-}
-
-//Determines optimized coordinates for each node in a network
-function setCoordinates() {
-    var grid_size = optimizeToGrid(nodes.length);
-    //Create 2D array
-    var coord_array = [];
-    var i;
-    for (i = 0; i < nodes.length; i++) {
-        coord_array[i] = [];
-        var x_coord = (i % grid_size[0]);
-        var y_coord = (Math.floor(i / grid_size[0]));    
-        coord_array[i][0] = x_coord;
-        coord_array[i][1] = y_coord;
-    }
-    return coord_array;
-}
-
-//Scales the coordinates relative to the center of canvas (plane)
-function scaleCoordinates(coord_array) {
-    var newCoordinates = [];
-    var x_max = 0;
-    var y_max = 0;
-
-    // return maximum values of x and y
-    var i;
-    for (i = 0; i < coord_array.length; i++) {
-        newCoordinates[i] = [];
-        if (coord_array[i][0] >= x_max) {
-            x_max = coord_array[i][0];
-        }
-
-        if (coord_array[i][1] >= y_max) {
-            y_max = coord_array[i][1];
-        }
-    }
-
-    // scaling by proportion (by maximum valus of x and y)
-    for (i = 0; i < coord_array.length; i++) {
-        newCoordinates[i][0] = coord_array[i][0]/x_max * 350 + 100; // random constant scaling
-        newCoordinates[i][1] = coord_array[i][1]/y_max * 150 + 50; // random constant scaling
-    }
-    return newCoordinates;
-}
-
-
-function hasTarget(id) {
-    var numSource = 0;
-    var numTarget = 0;
-    var i;
-    for (i = 0; i < edges.length; i++) { 
-        if (edges[i].source == id) {
-            return true;
-        }
-    }
-    return false;
 }
