d5af0043e7c7eaf32cc20574fd0d3da7ccbd1e8f
Andrew Fan <andrewsuicune@gmail.com>
2016-08-31 12:04:52 -0400

    Set up some framework to prepare for drawing

--- Phase 3/draw.js
+++ Phase 3/draw.js
@@ -6,7 +6,7 @@
  * General Notes:                                                            *
  * -All coordinates are handled in terms of PIXELS, not questionRowHeight    *
  * -This is entirely independent from the main black box and is used         *
- *  primarily for testing purposes.                                          *
+ *  primarily for testing purposes. Therefore, there may be hardcoded values *
  *************************************************************************** */
 
  "use strict";
@@ -18,5 +18,62 @@
 function render(inputfilename) {
     console.log("Beginning rendering onto canvas - input from " + inputfilename);
     var inputobj = sessionStorage[inputfilename];
-    console.log(inputobj);
+
+    var database_obj; //the JSON input
+
+    loadJSON();
+
+    /* ***********************************************************************
+     * void loadJSON()                                                       *
+     *                                                                       *
+     * This function loads the input JSON file to database_obj, and then     *
+     * runs processInput()                                                   *
+     *********************************************************************** */
+    function loadJSON() {
+        var client = new XMLHttpRequest();
+        client.open("GET", "survey.json", true);
+        client.onreadystatechange = function () { //callback
+            if (client.readyState === 4) {
+                if (client.status === 200 || client.status === 0) {
+                    database_obj = JSON.parse(client.responseText);
+                    processInput();
+                }
+            }
+        };
+        client.send();
+    }
+
+    /* ***********************************************************************
+     * void processInput()                                                   *
+     *                                                                       *
+     * This function runs immediately after the input JSON has been loaded   *
+     * into database_obj. It loads the appropriate data, then handles the    *
+     * rendering of the components to the canvas                             *
+     *********************************************************************** */
+    function processInput() {
+        handleQuestions();
+        handleEdges();
+    }
+
+    /* ***********************************************************************
+     * void handleQuestions()                                                *
+     *                                                                       *
+     * This function takes the input questions and renders them using the    *
+     * coordinates from the black box JSON                                   *
+     *********************************************************************** */
+
+    function handleQuestions() {
+
+    }
+
+    /* ***********************************************************************
+     * void handleEdges()                                                    *
+     *                                                                       *
+     * This function takes the input edges and renders them using the        *
+     * coordinates from the black box JSON                                   *
+     *********************************************************************** */
+
+    function handleEdges() {
+
+    }
 }
