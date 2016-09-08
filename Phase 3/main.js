f5f1de564905399423452bdb4827470af17ff4cb
Andrew Fan <andrewsuicune@gmail.com>
2016-09-08 14:06:18 -0400

    Fixed some weird white space usage

--- Phase 3/main.js
+++ Phase 3/main.js
@@ -602,23 +602,23 @@ function networkOptimization(inputfilename, outputfilename, jsoninput, canvas_si
             multipleLR += 1;
         }
 
-    // parametrize midpoints and prevent redundency
-//    var p1 = [sourcestubx + bestmultipleLR * sourcestub * curr_edge.sourceObject.questionRowHeight / 2, sourcey];
-    var p2 = [sourcestubx + bestmultipleLR * sourcestub * curr_edge.sourceObject.questionRowHeight / 2, targetstuby - bestmultipleTOP * curr_edge.targetObject.questionRowHeight / 2];
-    var p3 = [targetx, targetstuby - bestmultipleTOP * curr_edge.targetObject.questionRowHeight / 2];
-    var p4 = [targetx, targetstuby];
-    var p5 = [targetx, targety];
+        // parametrize midpoints and prevent redundency
+        //var p1 = [sourcestubx + bestmultipleLR * sourcestub * curr_edge.sourceObject.questionRowHeight / 2, sourcey];
+        var p2 = [sourcestubx + bestmultipleLR * sourcestub * curr_edge.sourceObject.questionRowHeight / 2, targetstuby - bestmultipleTOP * curr_edge.targetObject.questionRowHeight / 2];
+        var p3 = [targetx, targetstuby - bestmultipleTOP * curr_edge.targetObject.questionRowHeight / 2];
+        var p4 = [targetx, targetstuby];
+        var p5 = [targetx, targety];
 
-    // curr_edge.points.push(p1);
-    curr_edge.points.push(p2);
-    if (p2[0] !== p3[0]) {
-        curr_edge.points.push(p3);
-    }
-    if (p3[1] !== p4[1]) {
-        curr_edge.points.push(p4);
-    }
-    curr_edge.points.push(p5);
-    return mincollisions === Number.MAX_VALUE;
+        // curr_edge.points.push(p1);
+        curr_edge.points.push(p2);
+        if (p2[0] !== p3[0]) {
+            curr_edge.points.push(p3);
+        }
+        if (p3[1] !== p4[1]) {
+            curr_edge.points.push(p4);
+        }
+        curr_edge.points.push(p5);
+        return mincollisions === Number.MAX_VALUE;
     }
 
     /* ***********************************************************************
